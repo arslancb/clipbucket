05a265d4c7aaa77de3bd4b54f539bbed072333d6
Adrien Ponchelet <adrien.ponchelet@u-picardie.fr>
2016-10-25 15:58:07 +0200

    Correct install/uninstall path and small udpate

--- upload/plugins/waveform/install_waveform.php
+++ upload/plugins/waveform/install_waveform.php
@@ -6,7 +6,7 @@
 	function installWaveform() {
 		global $db;
 		
-		$sql = 'INSERT INTO '.tbl("expand_video_manager").' (`evm_id`, `evm_plugin_url`, `evm_zone`, `evm_is_new_tab`, `evm_tab_title`) VALUES (\'\', \'/var/www/html/cb_uhdf/upload/plugins/waveform/admin/mk_and_show_waveform.php\', \'expand_video_manager_right_panel\', 1, \'Waveform\');';
+		$sql = 'INSERT INTO '.tbl("expand_video_manager").' (`evm_id`, `evm_plugin_url`, `evm_zone`, `evm_is_new_tab`, `evm_tab_title`) VALUES (\'\', \''.BASEDIR.'/plugins/waveform/admin/mk_and_show_waveform.php\', \'expand_video_manager_right_panel\', 1, \'Waveform\');';
 		
  		//e($sql, "m");
 		
