e70d4ebd5c2d280cd6b20333b90e9a6bdfd20727
franck <franck@frk64>
2016-11-09 08:32:51 +0100

    bugfix when installing external link plugin from scratch

--- upload/plugins/external_links/install_external_links.php
+++ upload/plugins/external_links/install_external_links.php
@@ -9,8 +9,8 @@ global $cbplugin;
 if ($cbplugin->is_installed('common_library.php')){
 	require_once PLUG_DIR.'/common_library/common_library.php';
 	$folder= PLUG_DIR.'/'.basename(dirname(__FILE__))."/lang";
-	/*importLangagePack($folder,'en');
-	importLangagePack($folder,'fr');*/
+	importLangagePack($folder,'en');
+	importLangagePack($folder,'fr');
 	installPluginAdminPermissions("links", "External links administration", "Allow external links management");
 }
 
