5595d97928ee424aa7a6765c0b308f520047ded2
franck <franck@frk64>
2017-03-22 22:47:06 +0100

    Waveform	Plugin Bugfix : remove database entry fixed

--- upload/plugins/waveform/uninstall_waveform.php
+++ upload/plugins/waveform/uninstall_waveform.php
@@ -3,7 +3,7 @@
 function uninstallWaveform() {
 	global $db;
 	$db->Execute(
-	    'DELETE FROM `'.tbl("expand_video_manager").'` WHERE `expand_video_manager`.`evm_plugin_url` = \''.BASEDIR.'/plugins/waveform/admin/mk_and_show_waveform.php\';'
+	    'DELETE FROM '.tbl("expand_video_manager").' WHERE `evm_plugin_url` = \''.BASEDIR.'/plugins/waveform/admin/mk_and_show_waveform.php\';'
 	);
 }
 
